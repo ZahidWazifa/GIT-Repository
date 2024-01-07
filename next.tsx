8499d3c3c77b6e0aaf73f24dd9cddbf1ce5624f4
Zahid Wazifa <acerzifa@gmail.com>
2024-01-07 17:47:29 +0700

    update next.tsx

--- next.tsx
+++ next.tsx
@@ -2,6 +2,10 @@ const next = ()=>{
     return(
         <div>
         <h1>
-            Hello this is feature/a</h1></div>
+            Hello this is feature/a</h1>
+            <span>
+
+            </span>
+            </div>
     );
 }
